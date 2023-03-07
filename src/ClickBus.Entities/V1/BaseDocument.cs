606a756e374f0aebe51310a54745cb0dee090c1e
Guilherme Castro <guilhermecastro.developer@gmail.com>
2023-03-07 16:35:01 -0300

    Created initial structure to develop entities

---
+++ src/ClickBus.Entities/V1/BaseDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickBus.Entities.V1
+{
+    /// <summary>
+    /// TODO: Doc
+    /// </summary>
+    public class BaseDocument
+    {
+        public int Version => 1;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/ClickBus.Entities/V1/Place/PlaceEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace ClickBus.Entities.V1.Place
+{
+    /// <summary>
+    /// TODO: Doc.
+    /// </summary>
+    public class PlaceEntity : BaseDocument
+    {
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
